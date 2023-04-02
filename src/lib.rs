3f1163458fb79b5eb4eec937286de33581ea2f3f
John Bradbury <johnbradbury39@gmail.com>
2023-04-02 15:34:11 -0700

    tweak env

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,32 @@ pub use category::Category;
 
 pub use intent::Intent;
 use jni::{JNIEnv, JavaVM, AttachGuard};
+use ndk_context::AndroidContext;
+
+pub struct IntentEnv {
+    cx: AndroidContext,
+    vm: JavaVM,
+}
+
+impl IntentEnv {
+    pub fn new() -> Self {
+        let cx = ndk_context::android_context();
+        let vm = unsafe { JavaVM::from_raw(cx.vm().cast()) }.unwrap();
+
+        return Self {
+            cx,
+            vm,
+        };
+    }
+
+    pub fn get_env(&mut self) -> AttachGuard {
+        return self.vm.attach_current_thread().unwrap();
+    }
+}
 
 /// Run 'f' with the current [`JNIEnv`] from [`ndk_context`].
-pub fn with_current_env(f: impl FnOnce(AttachGuard)) {
-    let cx = ndk_context::android_context();
-    let vm = unsafe { JavaVM::from_raw(cx.vm().cast()) }.unwrap();
-    let env = vm.attach_current_thread().unwrap();
+pub fn with_current_env(intent_env: &mut IntentEnv, f: impl FnOnce(AttachGuard)) {
+    let env = intent_env.get_env();
 
     f(env);
 }
